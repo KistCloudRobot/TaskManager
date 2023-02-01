64df410c9e7d74e9e045576d6bf659a052ab00e1
insik <insik@172.16.165.141>
2023-02-01 16:04:46 +0900

    추가 안된 일부 플랜 추가, 실행 파일 추가

--- TaskManager_Local/src/demo/Lift1.java
+++ TaskManager_Local/src/demo/Lift1.java
@@ -3,13 +3,13 @@ package demo;
 import kr.ac.uos.ai.arbi.BrokerType;
 import taskManager.TaskManager;
 
-public class Local {
+public class Lift1 {
 
 	public static void main(String[] args) {
-		String role = "logisticManager";
-		String agentID = "agent://www.mcarbi.com/Local";
+		String role = "carrier";
+		String agentID = "agent://www.mcarbi.com/AMR_LIFT1";
 		String brokerAddress = "127.0.0.1";
-		int port = 61316;
+		int port = 61116;
 		BrokerType brokerType = BrokerType.ACTIVEMQ;
 		
 		TaskManager taskManager = new TaskManager(role, agentID, brokerAddress, port, brokerType);
--- TaskManager_Local/src/demo/Lift4.java
+++ TaskManager_Local/src/demo/Lift4.java
@@ -3,13 +3,13 @@ package demo;
 import kr.ac.uos.ai.arbi.BrokerType;
 import taskManager.TaskManager;
 
-public class Local {
+public class Lift4 {
 
 	public static void main(String[] args) {
-		String role = "logisticManager";
-		String agentID = "agent://www.mcarbi.com/Local";
+		String role = "carrier";
+		String agentID = "agent://www.mcarbi.com/AMR_LIFT4";
 		String brokerAddress = "127.0.0.1";
-		int port = 61316;
+		int port = 61113;
 		BrokerType brokerType = BrokerType.ACTIVEMQ;
 		
 		TaskManager taskManager = new TaskManager(role, agentID, brokerAddress, port, brokerType);
